58421204c6e660d1e76fc03bc3c3faa71b494de3
thegreatfeez <adamsafeez4@gmail.com>
2026-03-19 18:13:46 +0100

    fix wallet config and vite setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,39 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     nodePolyfills(),
+//   ],
+//   define: {
+//     'process.env': {},
+//     global: 'globalThis',
+//   },
+//   resolve: {
+//     alias: {
+//       process: 'process/browser',
+//       buffer: 'buffer',
+//       util: 'util',
+//     },
+//   },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//       define: {
+//         global: 'globalThis',
+//       },
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -10,20 +41,7 @@ export default defineConfig({
   ],
   define: {
     'process.env': {},
+    'process.browser': true,
     global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      buffer: 'buffer',
-      util: 'util',
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-    },
   },
 })
